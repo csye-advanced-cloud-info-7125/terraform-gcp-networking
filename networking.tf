e517d76b4644a51709a71c0be4513003d6a51397
Diksha Bhatia <90657593+bhatia-di@users.noreply.github.com>
2022-09-30 23:48:23 -0400

    Update networking.tf

--- networking.tf
+++ networking.tf
@@ -1,6 +1,6 @@
-resource "google_compute_address" "static" {
-  name = "ipv4-address"
-}
+# resource "google_compute_address" "static" {
+#   name = "ipv4-address"
+# }
 
 resource "google_compute_network" "vpc" {
   name                    = var.network
